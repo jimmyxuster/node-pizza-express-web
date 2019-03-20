9a01433bf115d6ead365ceefd7461c4dfafbf3b4
jimmyhsu <jimmyxuster@outlook.com>
2019-03-21 01:27:34 +0800

    update: static file dirs

--- config/config.default.js
+++ config/config.default.js
@@ -1,6 +1,7 @@
 /* eslint valid-jsdoc: "off" */
 
 'use strict';
+const path = require('path');
 
 /**
  * @param {Egg.EggAppInfo} appInfo app info
@@ -24,7 +25,20 @@ module.exports = appInfo => {
       port: 8080,
       hostname: '0.0.0.0',
     }
-};
+  };
+
+  config.static = {
+    dir: [
+      {
+        dir: path.join(appInfo.baseDir, 'app/public/images/'),
+        prefix: '/images/'
+      },
+      {
+        dir: path.join(appInfo.baseDir, 'app/public/'),
+        prefix: '/'
+      }
+    ]
+  }
 
   // add your user config here
   const userConfig = {
