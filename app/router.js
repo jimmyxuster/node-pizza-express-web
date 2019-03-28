d929f2aaab3130d155a5306d45d68f7fcd3bee26
jimmyhsu <jimmyxuster@outlook.com>
2019-03-28 22:51:25 +0800

    feat: alipay notify

--- app/router.js
+++ app/router.js
@@ -8,4 +8,5 @@ module.exports = app => {
   router.get('/', controller.home.index);
   router.put('/node_api/user', controller.user.update);
   router.post('/node_api/wapPay', controller.pay.wapPay);
+  router.post('/node_api/notify', controller.pay.notify);
 };
